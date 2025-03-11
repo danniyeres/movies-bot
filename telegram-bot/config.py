7d8015c67eca2589a309874ec2ec5146f96564dc
don <eleusizdaniyar777@gmail.com>
2025-03-11 18:56:53 +0500

    add .env file

--- telegram-bot/config.py
+++ telegram-bot/config.py
@@ -1,6 +1,6 @@
+from dotenv import load_dotenv
+import os
 
-BOT_TOKEN = "7746537029:AAEStDE86nMTrYC_Lx-Zbo-Dl8H9zWX1snk"
-
-API_GATEWAY_URL = "http://localhost:8080"
-USER_REGISTRATION_URL = f"{API_GATEWAY_URL}/api/users/register"
-STORAGE_URL = f"{API_GATEWAY_URL}/api/movies"
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_GATEWAY_URL = os.getenv("API_GATEWAY_URL")
